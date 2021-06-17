4bb4f834315d0b3a6befdadfbcb4b9b38f035f21
Kemo8965 <78011284+Kemo8965@users.noreply.github.com>
2021-06-17 03:10:26 +0200

    verifyTokens

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
+const Client = require('../models/Client');
 const { registerValidation, loginValidation } = require('../validation');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -96,5 +97,37 @@ router.get('/allUsers', async (req,res)=>{
           }
         }
       });
+
+      //CREATE NEW CLIENT
+router.post('/create/individualClient', async (req,res) => {
+    
+    try {  
+       const newClient = new Client({
+       firstName: req.body.firstName,
+       lastName: req.body.lastName,
+       title: req.body.title,
+       dateOfBirth: req.body.dateOfBirth,
+       gender: req.body.gender,
+       maritalStatus: req.body.maritalStatus,
+       idNumber: req.body.idNumber,
+       idType: req.body.idType,
+       phoneNumber: req.body.phoneNumber,
+       email: req.body.email,
+       residentialAddress: req.body.residentialAddress
+       });
+    
+       console.log(newClient);
+       
+      const savedClient = await newClient.save();
+        console.log(savedClient);
+            res.json({
+               
+                status: 'Successfully added a new client!'
+            });
+        } catch (err) {
+             res.json({ message: err })
+        }
+});
+
 
 module.exports= router
